6f17986a33a44baf1581958a3116616c5fb23e1c
matpazikas <matheuspazikas@gmail.com>
2024-09-21 14:04:48 -0300

    API Alterada

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const imageUrl = 'https://image.tmdb.org/t/p/w500';
 const kBackgoundColor = Color(0xff121012);
 const kbuttonColor = Color.fromARGB(255, 89, 54, 133);
-const apiKey = 'bc2eaf44d94625d51533a17bd9e4f1f9';
+const apiKey = '2e75005f1bca9d43ec63082907e425da';
 Color backgroundPrimary = const Color.fromARGB(255, 171, 169, 180);
